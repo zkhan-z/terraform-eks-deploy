bf5da6dde09fe7ac637c639494638874429ec84c
Zeshan khan <76880686+zkhan-z@users.noreply.github.com>
2021-09-19 16:02:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,10 +30,10 @@ module "vpc" {
   version = "2.58.0"
 
   name                 = "k8s-vpc"
-  cidr                 = "172.16.0.0/16"
+  cidr                 = # --> "ADD"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
-  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  private_subnets      = # --> ["ADD", "ADD", "ADD"] 
+  public_subnets       = # --> ["ADD", "ADD", "ADD"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
